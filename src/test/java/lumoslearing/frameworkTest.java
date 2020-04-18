5c71fc3e42598871f9c836fec85d9debcede2d74
archanaml08 <ml.archana905@gmail.com>
2020-04-18 14:02:30 +0530

    built frameworkTest

---
+++ src/test/java/lumoslearing/frameworkTest.java
@@ -0,0 +1,13 @@
+package lumoslearing;
+
+import org.testng.annotations.Test;
+
+public class frameworkTest {
+	
+	@Test
+	public void launchFramework()
+	{
+		System.out.println("Launching Framework");
+	}
+
+}
