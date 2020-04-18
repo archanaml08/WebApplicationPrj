1184c3fd231b182324ffd14be9105b25d8dd6bc8
archanaml08 <ml.archana905@gmail.com>
2020-04-18 12:23:54 +0530

    testcase updated

--- src/test/java/lumoslearing/SeleniumTest.java
+++ src/test/java/lumoslearing/SeleniumTest.java
@@ -8,11 +8,18 @@ public class SeleniumTest {
 	public void browserAutomation()
 	{
 		System.out.println("Browser Automation");
+		System.out.println("Web Automation");
 	}
 
 	@Test
 	public void elementUI()
 	{
 		System.out.println("Element UI");
+	}
+	
+	@Test 
+	public void logicflow()
+	{
+		System.out.println("Logic Flow Testing");
 	}
 }
