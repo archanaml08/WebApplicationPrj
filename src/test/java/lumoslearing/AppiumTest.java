260857dbe7ff94d842c3db9491acd29b7965a36c
archanaml08 <ml.archana905@gmail.com>
2020-04-18 12:36:39 +0530

    Updated Appium Functionality Test

--- src/test/java/lumoslearing/AppiumTest.java
+++ src/test/java/lumoslearing/AppiumTest.java
@@ -11,8 +11,20 @@ public class AppiumTest {
 	}
 	
 	@Test
+	public void nativeAppfunTest()
+	{
+		System.out.println("Mobile Native App fuctionality Testing");
+	}
+	
+	@Test
 	public void iosApp()
 	{
 		System.out.println("Mobile ios App Testing");
+	}
+	
+	@Test
+	public void iosAppfunTest()
+	{
+		System.out.println("Mobile ios App functionality Testing");
 	}
 }
